587af4291234e608c74a426adcc38ef4aa30f79f
Adam Miller <adam@pages.me>
2016-01-28 13:38:24 -0800

    Working to add country filter and password

--- api/google/contacts/index.js
+++ api/google/contacts/index.js
@@ -2,7 +2,7 @@ var Promise = require('bluebird');
 
 module.exports.GET = function(req, res) {
   return new Promise(function(resolve, reject){
-    res.locals.google.getFeed('https://www.google.com/m8/feeds/contacts/default/full', {'max-results':3},
+    res.locals.google.getFeed('https://www.google.com/m8/feeds/contacts/default/full', {'max-results':1000},
     function(e, feed) {
       if(e) return reject({status: 'error', message: e});
       resolve({statusn: 'success', data: feed});
